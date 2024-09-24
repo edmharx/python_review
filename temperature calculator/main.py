df14c2f74dcb0f65f9de7d9c1d97dec0fdf9bd1d
edmharx <edmharxx@gmail.com>
2024-09-24 17:52:54 +0800

    pseudo code

---
+++ temperature calculator/main.py
@@ -0,0 +1,8 @@
+#request user to input a number
+#ask the user if celsius to fahrenheit or vice versa
+#verify if the input is a float or int
+#convert
+#print answer
+#give option to do another conversion
+#put in while loop
+
