428acb83804fe5b2146e7fd900aab5223be3b69d
edmharx <edmharxx@gmail.com>
2024-09-26 18:43:04 +0800

    define ifs

--- temperature calculator/main.py
+++ temperature calculator/main.py
@@ -7,16 +7,26 @@
 #put in while loop
 
 
-num = float(input("Input the temperature to convert"))
-temp = input("Choose your conversion method")
+num = float(input("Input the temperature to convert: "))
+temp = input("Choose your conversion method: ")
+choices = ["celsius to fahrenheit", "fahrenheit to celsius"]
 
 def celsius_to_fahrenheit(to_convert_ct):
     ans = to_convert_ct * (9/5) + 32
-    print (ans)
+    print ("The answer is ",ans)
 
 def fahrenheit_to_celsius(to_convert_tc):
     ans = (to_convert_tc - 32) * 5/9
-    print (ans)
+    print ("The answer is ",ans)
 
 while True:
-    if temp == 1 or 2
+    if temp.lower in choices and temp == 1 or 2:
+        break
+    else:
+        print ("Type the conversion method or simply type the number: ")
+
+if temp == 1 or temp == choices[0]:
+    celsius_to_fahrenheit(num)
+elif temp == 2 or temp == choices[1]:
+    fahrenheit_to_celsius(num)
+
