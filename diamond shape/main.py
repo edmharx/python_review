23400473d5c71a40ae341ad00e32028245b18765
edmharx <edmharxx@gmail.com>
2024-09-27 19:56:19 +0800

    exception handling

---
+++ diamond shape/main.py
@@ -0,0 +1,14 @@
+#ask the width size
+#only integer number must be accepted
+# create the function
+#upper first -  same as to how to create a pyramid
+#lower part next - create a inverse pyramid
+
+width = int(input("Type the desired width of the diamond"))
+
+def print_diamond(width):
+        if width % 2 == 0:
+              print("Please provide an odd integer")
+        elif width < 0:
+              print("Type positive odd integer")
+        else: 
