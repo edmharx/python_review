19f7a866b31800df05028b291cad194a1b9059b9
edmharx <edmharxx@gmail.com>
2024-09-27 18:30:11 +0800

    print the missing value

--- ohm's law calculator/main.py
+++ ohm's law calculator/main.py
@@ -5,7 +5,13 @@
 #while loop
 
 def find_voltage(current, resistance):
+    ans = current * resistance
+    print("The value of voltage is ", ans)
 
 def find_current(voltage, resistance):
+    ans = voltage/resistance
+    print("The value of voltage is ", ans)
 
 def find_resistance(current, voltage):
+    ans = voltage/current
+    print("The value of voltage is ", ans)
