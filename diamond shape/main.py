df88b63638308f3f237f9016096feef0ba1cf901
edmharx <edmharxx@gmail.com>
2024-09-27 20:12:03 +0800

    define the upper part (pyramid)

--- diamond shape/main.py
+++ diamond shape/main.py
@@ -8,7 +8,10 @@ width = int(input("Type the desired width of the diamond"))
 
 def print_diamond(width):
         if width % 2 == 0:
-              print("Please provide an odd integer")
+            print("Please provide an odd integer")
         elif width < 0:
-              print("Type positive odd integer")
-        else: 
+            print("Type positive odd integer")
+        else:
+            for i in range(width):
+                print(' ' * (width - i - 1))
+                print('*' * (2 * i + 1))
