41de539e928ac2b9d9088bdf53275009b5d2667c
edmharx <edmharxx@gmail.com>
2024-09-27 15:56:56 +0800

    pseudo code and functions

---
+++ ohm's law calculator/main.py
@@ -0,0 +1,11 @@
+#ask what will be calculated
+#ask the appropriate values
+#print the missing value
+#give option to do another computation
+#while loop
+
+def find_voltage(current, resistance):
+
+def find_current(voltage, resistance):
+
+def find_resistance(current, voltage):
